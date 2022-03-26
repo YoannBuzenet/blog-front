bfd8c44a5b911b128f13c66ddaed185c87ba35f4
Yoann <ybuzenet@gmail.com>
2022-03-26 09:14:30 +0100

    api call works - should make it from the back end later

--- services/api/Post.js
+++ services/api/Post.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const getAllPosts = async () => {
-  const resp = await axios.get(`${process.env.API_URL}/api/posts`);
+  const resp = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/entities/posts`
+  );
 
   return resp.data;
 };
