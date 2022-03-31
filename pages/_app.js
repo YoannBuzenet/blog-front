5aee8e6f9144a8f32a0da6a5f5ba866a0731b409
Yoann <ybuzenet@gmail.com>
2022-03-31 20:19:31 +0200

    block quote css displayed !

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/generic/globals.scss";
 import config from "../config/axios";
 import "../styles/generic/normalize.css";
+import "../styles/generic/wysiwyg.scss";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
