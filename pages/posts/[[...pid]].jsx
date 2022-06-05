fc50e9afac74337f20cc0e34ae488c727b0d8559
Yoann Buzenet <ybuzenet@gmail.com>
2022-06-05 21:49:11 +0200

    ajout fonctionnalités eslint avec installation de BABEL PARSER + poc de page post OK

--- pages/posts/[[...pid]].jsx
+++ pages/posts/[[...pid]].jsx
@@ -1,3 +1,20 @@
-// présenter le post en détail ici
+import { getOnePost } from "../../services/api/Post";
+import { JSONParseAllProps } from "../../services/utils";
 
-// bien mettre la page dans un .container pour avoir le même layout
+export async function getServerSideProps({ req, query, params }) {
+  const { pid } = params;
+
+  // TODO : gérer si 0 article correspond
+  // Demander le post par le nom pour SEO purpose ?
+  const jsonPage = await getOnePost(pid);
+  const page = JSONParseAllProps(jsonPage);
+
+  return { props: { page } };
+}
+
+const Post = ({ page }) => {
+  console.log("page reçue", page);
+  return <div className="container">Post</div>;
+};
+
+export default Post;
