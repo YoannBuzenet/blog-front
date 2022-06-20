45647ba1d5eba4d8a2d4cfba1b1d752e79e8e229
Yoann Buzenet <ybuzenet@gmail.com>
2022-06-20 13:18:08 +0200

    more ccs navbar

--- components/Navbar/NavBar.jsx
+++ components/Navbar/NavBar.jsx
@@ -1,7 +1,18 @@
 import style from "../../styles/components/NavBar/NavBar.module.css";
 
 const NavBar = () => {
-  return <div className={style.navBarContainer}>Navbar</div>;
+  return (
+    <div className={style.navBarContainer}>
+      <div className={style.navBarDisplay}>
+        <div>
+          <p>Navbar</p>
+        </div>
+        <div>
+          <p>Menu</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
