53d6df9163bbeba1c6496c9de2b3e8f3d57c32f2
Yoann Buzenet <ybuzenet@gmail.com>
2022-06-20 23:32:10 +0200

    ajout lien peek post

--- components/posts/PostPeek.jsx
+++ components/posts/PostPeek.jsx
@@ -1,6 +1,7 @@
 import DisplayHTML from "../generic/wysiwyg/DisplayHTML";
 import { format } from "date-fns";
 import style from "../../styles/posts/PostPeek.module.css";
+import Link from "next/link";
 
 const PostPeek = ({ post }) => {
   console.log("mon post", post);
@@ -9,15 +10,17 @@ const PostPeek = ({ post }) => {
   // Le présenter dans les bons éléments pour un beau rendu
 
   return (
-    <div className={style.post}>
-      <DisplayHTML slateText={post.title} />
-      <p>Short description</p>
-      <DisplayHTML slateText={post.shortDescription} />
-      <p>Content</p>
-      <DisplayHTML slateText={post.content} />
-      <p>Publié le</p>
-      <p>{format(new Date(post.createdAt), "dd/MM/yyyy")} </p>
-    </div>
+    <Link href={`/posts/${post.id}`} passHref>
+      <div className={style.post}>
+        <DisplayHTML slateText={post.title} />
+        <p>Short description</p>
+        <DisplayHTML slateText={post.shortDescription} />
+        <p>Content</p>
+        <DisplayHTML slateText={post.content} />
+        <p>Publié le</p>
+        <p>{format(new Date(post.createdAt), "dd/MM/yyyy")} </p>
+      </div>
+    </Link>
   );
 };
 
