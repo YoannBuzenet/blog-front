0ece3a0b2ccbf4d5138a7176e3cab9ab40299c2a
Yoann <ybuzenet@gmail.com>
2022-10-12 22:26:09 +0200

    css research

--- components/myAccount/Answer.jsx
+++ components/myAccount/Answer.jsx
@@ -1,12 +1,29 @@
 import style from "../../styles/pages/myAccount/Answer.module.css";
+import DisplayHTML from "../generic/wysiwyg/DisplayHTML";
+import genericTextStyle from "../../styles/generic/genericTextStyle.module.css";
+import Link from "next/link";
 
 const Answer = ({ content }) => {
   console.log("content reçu", content);
   // answer in content.content
 
+  let contentParsed;
+  try {
+    contentParsed = JSON.parse(content.content);
+  } catch (e) {
+    console.log("Couldn't parse the answer.");
+  }
+
   return (
     <div className={style.container}>
-      <p>Answer</p>
+      <div>
+        <div className={`${genericTextStyle.content} ${style.answerContent}`}>
+          <DisplayHTML slateText={contentParsed} />
+        </div>
+        <div className={`${style.link} generalLink`}>
+          <Link href={`/posts/${content.PostId}`}>See the article</Link>
+        </div>
+      </div>
     </div>
   );
 };
