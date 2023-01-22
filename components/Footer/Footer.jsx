5da18c2d7c660e2cb7a12079c0db05ccec20c060
Yoann <ybuzenet@gmail.com>
2023-01-22 20:51:53 +0100

    exploring home page css

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,11 +1,31 @@
 "use client";
-import style from "../../styles/components/Footer/Footer.module.css";
+import style from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <div className={`${style.footer}`}>
       <div className={`${style.footerContent} container`}>
-        <p>Footer</p>
+        <div
+          className={`h3 ${style.footerContent__section} ${style.footerContent__firstColumn}`}
+        >
+          <h3>General</h3>
+          <p>© Rights</p>
+        </div>
+        <div className={`h3 ${style.footerContent__section}`}>
+          <h3>About</h3>
+          <p>General</p>
+          <p>Rights</p>
+        </div>
+        <div className={`h3 ${style.footerContent__section}`}>
+          <h3>Resources</h3>
+          <p>General</p>
+          <p>Rights</p>
+        </div>
+        <div className={`h3 ${style.footerContent__section}`}>
+          <h3>Social</h3>
+          <p>General</p>
+          <p>Rights</p>
+        </div>
       </div>
     </div>
   );
