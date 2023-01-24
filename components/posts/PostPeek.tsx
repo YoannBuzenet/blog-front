a9911fdbed87e5f14ce09254cea791d6dd9be05b
Yoann <ybuzenet@gmail.com>
2023-01-24 21:31:43 +0100

    css improvment

--- components/posts/PostPeek.tsx
+++ components/posts/PostPeek.tsx
@@ -4,17 +4,33 @@ import style from "../../styles/posts/PostPeek.module.scss";
 import Link from "next/link";
 
 import { previewImageUrl } from "../../services/imageUtils";
+import { Post } from "../../domain/post/Post";
 
 const PostPeek = ({ post }) => {
+  const postDomain = Post.builder()
+    .id(post.id)
+    .mainImageUrl(post.mainImageUrl)
+    .title(post.title)
+    .createdAt(post.createdAt)
+    .shortDescription(post.shortDescription)
+    .build();
+
   return (
-    <Link href={`/posts/${post.id}`} passHref className={style.postContainer}>
+    <Link
+      href={`/posts/${postDomain.id}`}
+      passHref
+      className={style.postContainer}
+    >
       <div className={style.post}>
         <div>
           <div className={style.imageContainer}>
-            <img src={previewImageUrl(post.mainImageUrl)} />
+            <img src={previewImageUrl(postDomain.mainImageUrl)} />
           </div>
         </div>
-        <DisplayHTML slateText={post.title} />
+        <DisplayHTML slateText={postDomain.title} />
+        <p>
+          <DisplayHTML slateText={postDomain.shortDescription} />
+        </p>
         <p className={style.articleDate}>
           {format(new Date(post.createdAt), "dd/MM/yyyy")}{" "}
         </p>
