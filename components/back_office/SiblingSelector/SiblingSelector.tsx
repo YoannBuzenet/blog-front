644662d2c5687153c934fb9fdbbea9bdd3fcdc63
Yoann <ybuzenet@gmail.com>
2023-02-09 22:31:22 +0100

    article are now displayed with their names in react-select

--- components/back_office/SiblingSelector/SiblingSelector.tsx
+++ components/back_office/SiblingSelector/SiblingSelector.tsx
@@ -4,8 +4,8 @@ import style from "./SiblingSelector.module.scss";
 import Select from "react-select";
 import { useCallback, useState } from "react";
 import { debounce } from "../../../services/utils";
-import axios from "axios";
 import { getOnePostbyTitle } from "../../../services/api/post";
+import { parseSlateFormatSimple } from "../../../services/react-slate";
 
 // Refacto step: nom de la prop languageAvailables pas clair car cache le fait que c'est une liste d'objet compatible avec les options de react-select -> typer ?
 
@@ -17,23 +17,22 @@ const SiblingSelector = ({ languageAvailables, pageState, setPageState }) => {
   const languageNotUsed = languageAvailables.filter(
     (language) => language.value !== pageState.language
   );
-  console.log(languageNotUsed, "languageNotUsed");
-  console.log(langSibblingSelected, "langSibblingSelected");
-
-  // TODO parser la réponse, la mettre au format option React Select
-  // le JSON du wysiwyg doit être parsé pour les labels ahah
 
   const debounceGetPostsByTitle = useCallback(
     debounce((inputValue, currentLang) => {
       const currentLangValue = currentLang.value;
       return getOnePostbyTitle(inputValue, true, currentLangValue).then(
-        (resp) => setArticleAvailables(resp)
+        (resp) =>
+          setArticleAvailables(
+            resp.map((article) => ({
+              value: article.id,
+              label: parseSlateFormatSimple(JSON.parse(article.title)),
+            }))
+          )
       );
     }, 1000),
     []
   );
-
-  console.log(":O", articleAvailables);
 
   return (
     <div className={style.container}>
@@ -49,7 +48,7 @@ const SiblingSelector = ({ languageAvailables, pageState, setPageState }) => {
       </div>
       <div className={style.container__rightDiv}>
         <Select
-          options={[]}
+          options={articleAvailables}
           isSearchable
           onInputChange={(value) => {
             setInputSearch(value);
