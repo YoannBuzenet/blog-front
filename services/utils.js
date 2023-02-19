dcb79c26a00d9a0cda778af5bb903c19953fe607
Yoann <ybuzenet@gmail.com>
2023-02-19 14:05:04 +0100

    slug correction + css improvment

--- services/utils.js
+++ services/utils.js
@@ -129,16 +129,24 @@ function debounce(func, wait, immediate) {
 const slugify = (...args) => {
   const value = args.join(' ')
 
-  return value
+  let slug =  value
       .normalize('NFD') // split an accented letter in the base letter and the acent
       .replace(/[\u0300-\u036f]/g, '') // remove all previously split accents
       .toLowerCase()
       .trim()
       .replace(/[^a-z0-9 ]/g, '') // remove all chars not letters, numbers and spaces (to be replaced)
       .replace(/\s+/g, '-') // separator
+
+  if(slug.endsWith("-")){
+      slug = slug.substring(0, slug.length - 1)
+  }    
+
+  return slug;
 }
   
-
+module.exports = {
+  slugify
+}
 
 
 
