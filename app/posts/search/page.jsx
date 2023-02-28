93cd4eb1aeffcd59fa5b4882f901a290af34bdee
Yoann <ybuzenet@gmail.com>
2023-02-28 20:12:33 +0100

    tags are now updated client side if needed

--- app/posts/search/page.jsx
+++ app/posts/search/page.jsx
@@ -9,7 +9,7 @@ export default async function PostSearch({ params }) {
 
   return (
     <>
-      <SearchPost initialTags={tagsLangBrower} />
+      <SearchPost initialTags={tagsLangBrower} localeBrowser={localeBrowser} />
     </>
   );
 }
--- components/posts/Search/SearchPost.tsx
+++ components/posts/Search/SearchPost.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import AppLangContext from "../../../contexts/appCurrentLang";
+import { getAllPostTags } from "../../../services/api/tag";
 
-const SearchPost = ({ initialTags }) => {
+const SearchPost = ({ initialTags, localeBrowser }) => {
   const { appCurrentLang } = useContext(AppLangContext);
+  const [tagsAppLanguage, setTagsAppLanguage] = useState(initialTags);
+
+  useEffect(() => {
+    if (!appCurrentLang.isDefault && appCurrentLang.locale !== localeBrowser) {
+      console.log("Updating tag language");
+      getAllPostTags(appCurrentLang.locale).then((resp) =>
+        setTagsAppLanguage(resp)
+      );
+    }
+  }, []);
 
   // TODO Chercher les tags depuis la langue du contexte et feed le multi select
 
