066544b2d43643a21628463f3a3f838a514619fd
Yoann <ybuzenet@gmail.com>
2023-03-01 10:07:06 +0100

    multi select works

--- app/posts/search/page.jsx
+++ app/posts/search/page.jsx
@@ -1,6 +1,7 @@
 import SearchPost from "../../../components/posts/Search/SearchPost";
 import { headers } from "next/headers";
 import { getAllPostTags } from "../../../services/api/tag";
+import { getLangFromHeaders } from "../../../services/i18n";
 
 export default async function PostSearch({ params }) {
   const localeBrowser = getLangFromHeaders(headers);
--- components/posts/Search/SearchPost.tsx
+++ components/posts/Search/SearchPost.tsx
@@ -3,6 +3,7 @@
 import { useContext, useEffect, useState } from "react";
 import AppLangContext from "../../../contexts/appCurrentLang";
 import { getAllPostTags } from "../../../services/api/tag";
+import MultipleSelectChip from "../../generic/MultiSelect/MultiSelect";
 
 const SearchPost = ({ initialTags, localeBrowser }) => {
   const { appCurrentLang } = useContext(AppLangContext);
@@ -19,9 +20,29 @@ const SearchPost = ({ initialTags, localeBrowser }) => {
 
   // TODO Chercher les tags depuis la langue du contexte et feed le multi select
 
+  const [personName, setPersonName] = useState<string[]>([]);
+
+  const names = [
+    "Oliver Hansen",
+    "Van Henry",
+    "April Tucker",
+    "Ralph Hubbard",
+    "Omar Alexander",
+    "Carlos Abbott",
+    "Miriam Wagner",
+    "Bradley Wilkerson",
+    "Virginia Andrews",
+    "Kelly Snyder",
+  ];
+
   return (
     <div className="largePageContainer belowNavbar">
       <p>Search Post</p>
+      <MultipleSelectChip
+        initialListElements={names}
+        selectedElements={personName}
+        setSelectedElements={setPersonName}
+      />
     </div>
   );
 };
