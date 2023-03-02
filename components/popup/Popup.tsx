281f428678db5428a9729be184a6c3a0edbbf782
Yoann <ybuzenet@gmail.com>
2023-03-02 20:51:54 +0100

    function to remove pop ups

--- components/popup/Popup.tsx
+++ components/popup/Popup.tsx
@@ -1,5 +1,7 @@
-import { FC, ReactElement } from "react";
+import { FC, useContext } from "react";
 import style from "./PopUp.module.scss";
+import PopUpsDisplayedContext from "../../contexts/popUpsDisplay";
+import CloseMenu from "../../assets/svg/close/outline.svg";
 
 type PopupProps = {
   index: number;
@@ -8,6 +10,15 @@ type PopupProps = {
 
 const Popup = ({ index, CompoToRender }: PopupProps) => {
   const idPopUp = `popup-${index}`;
+  const { popUpsDisplayed, setPopUpsDisplayed } = useContext(
+    PopUpsDisplayedContext
+  );
+
+  const handleClosePopUp = () => {
+    const arrayPopUpCopy = [...popUpsDisplayed];
+    arrayPopUpCopy.splice(index, 1);
+    setPopUpsDisplayed(arrayPopUpCopy);
+  };
 
   return (
     <div className={`${idPopUp} ${style.popUpContainer}`}>
@@ -16,8 +27,22 @@ const Popup = ({ index, CompoToRender }: PopupProps) => {
           z-index: ${100 + index};
         }
       `}</style>
-      <p>Je suis un pop up</p>
-      {CompoToRender && <CompoToRender />}
+      <div>
+        <div className={`${style.topPart}`}>
+          <div className={`${style.topPart__leftPart}`}></div>
+          <div className={`${style.topPart__rightPart}`}>
+            <CloseMenu
+              className={style.closeSvg}
+              title="Fermer"
+              onClick={handleClosePopUp}
+            />
+          </div>
+        </div>
+        <div className={`${style.mainPart}`}>
+          <p>Je suis un pop up</p>
+          {CompoToRender && <CompoToRender />}
+        </div>
+      </div>
     </div>
   );
 };
